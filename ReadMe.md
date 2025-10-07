72d41cfe1a359d956bb6d4805fea62f302876512
Patrick Moon <patrickmoon@outlook.com>
2025-10-07 10:07:36 -0700

    adds an icon and how we made the conversion.

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,6 @@
+<p align="center">
+  images/evergreen.png
+</p>
+
+#if you are doing the ps1 version you need this:
 Set-ExecutionPolicy -Scope CurrentUser Unrestricted
---
+++ convert.ps1
@@ -0,0 +1,5 @@
+Import-Module ps2exe
+Invoke-ps2exe -inputFile "C:\Users\Public\Projects\GitHub\nl_no_ie\WoodManager.ps1" `
+              -outputFile "C:\Users\Public\Projects\GitHub\nl_no_ie\WoodManager.exe" `
+              -iconFile "C:\Users\Public\Projects\GitHub\nl_no_ie\WoodManager.ico" `
+              -noConsole
